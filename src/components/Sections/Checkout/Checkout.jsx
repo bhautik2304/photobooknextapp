0b162aac42026d211d8e1a5aac90c864746d8c68
bhautik2304 <rathodbhautikrameshbhai@gmail.com>
2024-03-23 18:10:24 +0530

    Ready To Testing

--- src/components/Sections/Checkout/Checkout.jsx
+++ src/components/Sections/Checkout/Checkout.jsx
@@ -96,8 +96,8 @@ function Checkout() {
     if (file.type == "application/x-zip-compressed") {
       const UPLOAD_FILE_SIZE_LIMIT = 500 * 1024 * 1024;
       
-      /* Change hear dropbox */
-      
+      /* Change hear */
+      const ACCESS_TOKEN = "sl.Bx_sCAUxXmMn_l4rSRe2T1bIh3cSXeQVTLcPJcpmQJnTOO1gYyM-VMC-LykTlElGM7Yu0gsou6yrrItBgx2S45u3fKU_jDCa3Q3sUicwC1VtXKA49StcYm1AqZ_IZ4la67g_lMbCbrapBOBnP2jq-wU";
       
       const dbx = new Dropbox({ accessToken: ACCESS_TOKEN });
       let progreShBar = 0;
